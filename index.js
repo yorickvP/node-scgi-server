da433722ea4556404e593dbbd4ff928aecf54670
Yorick <yorickvanpelt@gmail.com>
2011-10-02 16:02:14 +0200

    Add ISC license

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /*jshint asi:true laxbreak:true */
 /* requires node-protoparse (npm install protoparse).
- * or just install this using npm install scgi-server */
+ * or just install this using npm install scgi-server
+----------------------------------------------------------------------------
+Copyright (c) YorickvP (contact me on github if you want)
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * ---------------------------------------------------------------------------*/
 var    net = require('net')
   , Parser = require('protoparse')
  , EventEmitter = require('events').EventEmitter
